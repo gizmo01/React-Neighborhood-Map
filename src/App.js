011b3859c2b2b7e398192e18ca39e752ee9fb992
Mario Amore <quaaludes@gmail.com>
2018-08-05 14:16:42 +0800

    Create Map component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <main className='main-page'>
+          <div className='right-section'>
+            <section tabIndex='0'>
 
+              <Map
+                venueName={showingVenueName}
+                onToggleOpen={this.onToggleOpen}
+                venue={showingVenueName}
+                getInfo={this.getInfo}
+                infoContent={this.state.infoContent}
+                placeToShow={this.state.placeToShow}
+                isOpen={this.state.isOpen}
+                containerElement={<div className='containerElement'/>}
+                mapElement={<div className='mapElement' />}
+                loadingElement={<div className='loadingElement' />}
+                googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyB6KuC_x__0u1uAEBxlPIb1LL-r7U8nFwQ&v=3'//rimettere la mia piu tardi
+              />
+
+            </section>
+          </div>
+        </main>
       </div>
     );
   }
---
+++ src/Map.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+const { compose } = require("recompose");
+
+
+const Map = compose(
+  withScriptjs,
+  withGoogleMap
+)(props =>
+
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{lat: -22.941648,lng: -43.209736}}
+  >
+
+    
+
+  </GoogleMap>
+);
+
+export default Map
