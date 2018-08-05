8d4cc1ecc3b392c7518706a30b42531a169f0c23
Mario Amore <quaaludes@gmail.com>
2018-08-05 14:12:38 +0800

    Setting state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import * as locations from './locations.json';
 
 
 class App extends Component {
+  state = {
+    venue: locations,
+    searchedVenues: '',
+    placeToShow: '',
+    isOpen: false,
+    error: false,
+    infoContent: ""
+  }
   render() {
     return (
       <div className="App">
+
       </div>
     );
   }
